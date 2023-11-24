29dbdb287a9faa4edb85101acc90831b9cb0b25a
SHADIMK7 <muhammedshadi.mk7@gmail.com>
2023-11-24 18:15:11 +0530

    first-shadi

--- Backend/settings.py
+++ Backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'owner_app',
 ]
 
 MIDDLEWARE = [
--- Backend/urls.py
+++ Backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('owner/', include('owner_app.urls'))
 ]

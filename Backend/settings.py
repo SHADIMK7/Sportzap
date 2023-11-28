07e94f0439af90fa6219cceb0d04fb264adb583c
Arjun Vinod M <marjunvinod@gmail.com>
2023-11-28 14:13:22 +0530

    user commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -84,16 +84,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sportzap',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost', 
-        'PORT': '5432',  
-}
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sportzap',
+#         'USER': 'postgres',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost', 
+#         'PORT': '5432',  
+# }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- owner_app/models.py
+++ owner_app/models.py
@@ -5,10 +5,6 @@ from django.contrib.auth.models import AbstractUser,Group,Permission
 class Owner(AbstractUser):
     Organization_name = models.CharField(max_length=20)
     Phone_number = models.CharField(max_length=10)
-<<<<<<< HEAD
-    groups = models.ManyToManyField(Group, related_name='owner_groups')
-    user_permissions = models.ManyToManyField(Permission, related_name='owner_user_permissions')
-=======
 
 
 class Amenity(models.Model):
@@ -28,4 +24,3 @@ class Turf(models.Model):
         
     def _str_(self):
         return self.turf_name
->>>>>>> 7cd038dad71ff8655aa4868d9d92f099cd245876

f8ed0d66524464de957ee8acecdd59cd33e6a707
Akshaya <akshayasekharan21@gmail.com>
2023-11-29 16:57:40 +0530

    turf-customer-management

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
         path('owner_list/',OwnerList.as_view(), name='owner_list'),
         path('owner_list/<str:username>/',OwnerDelete.as_view(), name='owner_list'),
         path('turf_list/',TurfList.as_view(), name='turf_list'),
-        path('turf_list/<str:username>/',TurfDelete.as_view(), name='turf_list'),
+        path('turf_list/<int:id>/',TurfActiveDelete.as_view(), name='turf_list'),
+        path('customer_list/',CustomerList.as_view(), name='customer_list'),
+        path('booking_list/',TurfBookingView.as_view(), name='booking_list'),
+        # path('admin_view/',AdminDataView.as_view(), name='admin_view'),
 
 
 ]
--- admin_app/views.py
+++ admin_app/views.py
@@ -6,7 +6,12 @@ from rest_framework.views import APIView
 from rest_framework import status
 from admin_app.serializers import IncomeSerializer
 from rest_framework import generics,mixins
-
+from user_app.serializers import RegisterUserSerializer,BookingSerializer
+from user_app.models import Customer,TurfBooking
+from admin_app.serializers import TurfUpdateSerializer,CustomerListSerializer
+from django.http import Http404
+from datetime import datetime, timedelta
+from django.db.models import Sum
 
 
 # Create your views here.
@@ -31,40 +36,63 @@ class TurfList(generics.ListAPIView):
     queryset = Turf.objects.all()
     serializer_class = TurfSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+   
 
-
-class TurfDelete(mixins.DestroyModelMixin,generics.GenericAPIView):
+class TurfActiveDelete(APIView):
     queryset = Turf.objects.all()
     serializer_class = TurfSerializer
-
+    lookup_field = 'id'
+    def get_object(self):
+        try:
+            return self.queryset.get(pk=self.kwargs[self.lookup_field])
+        except Turf.DoesNotExist:
+            raise Http404
     def delete(self, request, *args, **kwargs):
-       return self.destroy(request, *args, **kwargs)
+        try:
+            instance = self.get_object()
+            instance.delete()
+            return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Turf.DoesNotExist:
+            return Response({"message": "Object does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = True  
+        instance.save()
+        serializer = self.serializer_class(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class CustomerList(generics.ListAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerListSerializer
+    
+class TurfBookingView(APIView):
+    def get(self,request):
+        booking= TurfBooking.objects.all()
+        serializer= BookingSerializer(booking,many=True)
+        return Response(serializer.data)
+    
 
 # class AdminDataView(APIView):
 #     def get(self, request):
-#             reversed_court = Court_booking.objects.all().order_by('-id')
+#             reversed_court = TurfBooking.objects.all().order_by('-id')
 #             current_date = datetime.now()
             
 #             first_day = current_date.replace(day=1)
 #             last_day = (current_date.replace(day=1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)
-#             income = Court_booking.objects.filter(
+#             income = TurfBooking.objects.filter(
 #                 date__range=[first_day, last_day], payment_status='SUCCESS'
 #             ).aggregate(total=Sum('price'))['total'] or 0
             
 #             year_first_day = current_date.replace(month=1, day=1)
 #             year_last_day = current_date.replace(month=12, day=31)
             
-#             yearly_income = Court_booking.objects.filter(
+#             yearly_income = TurfBooking.objects.filter(
 #                 date__range=[year_first_day, year_last_day], payment_status='SUCCESS'
 #             ).aggregate(total=Sum('price'))['total'] or 0 
             
-#             reversed_court_status = Court_booking.objects.filter(payment_status='SUCCESS').order_by('-id')
+#             reversed_court_status = TurfBooking.objects.filter(payment_status='SUCCESS').order_by('-id')
             
 #             data = {
 #                 'income': income,
@@ -74,5 +102,5 @@ class TurfDelete(mixins.DestroyModelMixin,generics.GenericAPIView):
 #             }
             
 #             return Response(data)
-#         
+        
  

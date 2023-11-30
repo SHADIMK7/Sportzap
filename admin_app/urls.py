8b1a469620ce07820ca47cb78ba387209c580c2c
Arjun Vinod M <marjunvinod@gmail.com>
2023-11-30 11:14:09 +0530

    arj

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         path('turf_list/',TurfList.as_view(), name='turf_list'),
         path('turf_list/<int:id>/',TurfActiveDelete.as_view(), name='turf_list'),
         path('customer_list/',CustomerList.as_view(), name='customer_list'),
-        path('booking_list/',TurfBookingView.as_view(), name='booking_list'),
+        # path('booking_list/',TurfBookingView.as_view(), name='booking_list'),
         # path('admin_view/',AdminDataView.as_view(), name='admin_view'),
 
 

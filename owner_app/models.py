52963f18c3ddec8cdea53f2710b53ab87f4dfe49
SHADIMK7 <muhammedshadi.mk7@gmail.com>
2023-11-30 12:44:46 +0530

    MatchRating

--- owner_app/models.py
+++ owner_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,Group,Permission
 from user_app.models import *
-from user_app.models import Customer
+from user_app.models import Customer, Team
 
 # Create your models here.
 class Owner(AbstractUser):
@@ -24,7 +24,7 @@ class Turf(models.Model):
     image = models.ImageField(upload_to='image/') 
     description = models.CharField(max_length=255)
     amenity = models.ManyToManyField(Amenity)
-    is_active = models.BooleanField(default=False)
+    # is_active = models.BooleanField(default=False)
 
     def _str_(self):
         return self.name
@@ -53,3 +53,20 @@ class PaymentHistoryModel(models.Model):
     turf_booking = models.ForeignKey(TurfBooking, on_delete=models.SET_NULL, null=True)
     turf = models.ForeignKey(Turf, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(Customer,on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.user.username + "has booked " + self.turf.name 
+
+
+class MatchModel(models.Model):
+    team1 = models.ForeignKey(Team, related_name='team1', on_delete=models.CASCADE)
+    team2 = models.ForeignKey(Team, related_name='team2', on_delete=models.CASCADE)
+    team1_score = models.IntegerField(default=0)
+    team2_score = models.IntegerField(default=0)
+    date_played = models.DateField()
+
+
+class MatchRatingModel(models.Model):
+    match = models.ForeignKey(MatchModel, on_delete=models.CASCADE)
+    turf = models.ForeignKey(Turf, on_delete=models.CASCADE)
+    remark = models.CharField(max_length=50, null=True)

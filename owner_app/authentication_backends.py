bb0ed47f2fe2282efeb056400dba8affb323db7b
Arjun Vinod M <marjunvinod@gmail.com>
2023-12-04 10:03:40 +0530

    updated-1

--- owner_app/authentication_backends.py
+++ owner_app/authentication_backends.py
@@ -8,12 +8,8 @@ class EmailOrUsernameModelBackend(ModelBackend):
         try:
             user = UserModel.objects.get(email=username)
         except UserModel.DoesNotExist:
-<<<<<<< HEAD
             # If the username is not an email, try to get the user by username
             user = UserModel.objects.get(phone_no=username)
-=======
-            user = UserModel.objects.get(username=username)
->>>>>>> 96d3b44411a3984f002cd2ac26039bf3985f2e92
 
         if user.check_password(password):
             return user

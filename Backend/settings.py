036d07d3fcc81d5532d66379dfc9a596bd8441e6
Akshaya <akshayasekharan21@gmail.com>
2023-12-22 14:36:43 +0530

    notification

--- Backend/settings.py
+++ Backend/settings.py
@@ -117,16 +117,16 @@ DATABASES = {
 }
 
 # print("getttttttt",os.getenv("DB_NAME"))
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':os.getenv("DB_NAME"),
-#         'USER':os.getenv("DB_USER"),
-#         'PASSWORD':os.getenv("DB_PASSWORD"),
-#         'HOST':os.getenv("DB_HOST"),
-#         'PORT':os.getenv("DB_PORT"),
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':os.getenv("DB_NAME"),
+        'USER':os.getenv("DB_USER"),
+        'PASSWORD':os.getenv("DB_PASSWORD"),
+        'HOST':os.getenv("DB_HOST"),
+        'PORT':os.getenv("DB_PORT"),
+   }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
